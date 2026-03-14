501a33ffa09391a4013cb82e29f30053447b4729
siny2000 <sundress198@gmail.com>
2026-03-15 00:08:36 +0700

    initial be

---
+++ be-cleaning/src/main/java/com/clever/clean/clever_clean_service/BeCleaningApplication.java
@@ -0,0 +1,13 @@
+package com.clever.clean.clever_clean_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCleaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCleaningApplication.class, args);
+	}
+
+}
